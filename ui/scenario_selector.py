9556f237dcbb772356a4a912aee857b971613906
severinkl <severin.kliegl@gmail.com>
2025-08-29 09:19:58 +0200

    Update scenario button texts

--- ui/scenario_selector.py
+++ ui/scenario_selector.py
@@ -109,9 +109,9 @@ class ScenarioSelector:
         )
 
         scenarios = [
-            ("HTTP (ORF) Level 1", "http_level_1"),
-            ("HTTP (ORF) Level 2", "http_level_2"),
-            ("HTTP (ORF) Level 3", "http_level_3"),
+            ("ORF Phase 1 Newbie", "http_level_1"),
+            ("ORF Phase 2 Krypto", "http_level_2"),
+            ("ORF Phase 3 NWT Lab", "http_level_3"),
         ]
 
         for idx, (name, scenario_id) in enumerate(scenarios, start=1):
